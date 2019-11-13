221b4a5a66511e926c673a904fc4ce35220a8f66
Student <example@example.com>
2019-11-13 15:02:26 -0800

    change styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,35 +10,71 @@ function NavBar() {
           <li className='btn home-button'>EVERLANE</li>
         </ul>
       </div>
-      <div className='row bottom nav-row'>
-        <div className='left-cluster'>
-          
+
+
+    <div className='row bottom nav-row'>
+      <div className='left-cluster'>
+        <div className='dropdown-toggle'>
+          <button className='btn' type='button' data-toggle='collapse' data-target='#collapseExample' aria-expanded='false' aria-controls='collapseExample'>
+            Women
+          </button>
+          <button className='btn' type='button' data-toggle='collapse' data-target='#collapseExample2' aria-expanded='false' aria-controls='collapseExample'>
+            Men
+          </button>
+          <button className='btn' type='button' data-toggle='collapse' data-target='#collapseExample3' aria-expanded='false' aria-controls='collapseExample'>
+            About
+          </button>
+        </div>
+
+        <div className='collapse' id='collapseExample'>
+          <div className='card card-body'>
+            <ul>
+              <li>Sweaters</li>
+              <li>Overalls</li>
+              <li>Pants</li>
+              <li>Jackets</li>
+              <li>Hats</li>
+              <li>Shoes</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='collapse' id='collapseExample2'>
+          <div className='card card-body'>
+            <ul>
+              <li>Sweaters</li>
+              <li>Overalls</li>
+              <li>Pants</li>
+              <li>Jackets</li>
+              <li>Hats</li>
+              <li>Shoes</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='collapse' id='collapseExample3'>
+          <div className='card card-body'>
+            Here at Everlane, we really care about making sure our sweaters are extremely expensive.
+          </div>
+        </div>
+
+
         </div>
         <div className='right-cluster'>
           <p>
-          <button className='btn shopping-icon'><i className='fas fa-shopping-bag'></i></button>
+            <button className='btn shopping-icon'><i className='fas fa-shopping-bag'></i></button>
           </p>
         </div>
 
 
       </div>
 
-<p>
-  <button className='btn btn-primary' type='button' data-toggle='collapse' data-target='#collapseExample' aria-expanded='false' aria-controls='collapseExample'>
-    Button with data-target
-  </button>
-</p>
-<div className='collapse' id='collapseExample'>
-  <div className='card card-body'>
-    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-  </div>
-</div>
 
 
 
 
 
-      
+
     </div>
   );
 }
